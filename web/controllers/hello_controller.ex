9af644ae74ddfd31e3226338dc20aa6b8378e1ac
Marcle Rodrigues <maarclee@gmail.com>
2016-10-22 23:10:27 -0300

    Add a dynamic route and make use of template engine

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -4,4 +4,10 @@ defmodule TestApp.HelloController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  # If need to access all the params just pattern match like this:
+  # show(conn, %{"messenger" => messenger} = params)
+  def show(conn, %{"messenger" => messenger}) do
+    render conn, "show.html", messenger: messenger
+  end
 end
