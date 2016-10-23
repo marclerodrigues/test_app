4d4b0c467259da2c65d1893a9f97315c98f9778d
Marcle Rodrigues <maarclee@gmail.com>
2016-10-22 23:53:29 -0300

    Play around with the Phoenix.Router

--- web/router.ex
+++ web/router.ex
@@ -19,7 +19,29 @@ defmodule TestApp.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+    # resources "/users", UserController do
+    #   resources "/comments", CommentsController
+    # end
   end
+
+  # The router is perfectly fine with two scopes defined for the same path
+  scope "/", AnotherApp do
+    pipe_through :browser
+
+    get "/posts", PostController, :index
+  end
+
+  scope "/admin", TestApp.Admin, as: :admin do
+    pipe_through :browser
+
+    resources "/users", UserController
+  end
+
+  # Custom pipelines
+  # pipeline :review_checks do
+  #   plug :ensure_authenticated_user
+  #   plug :ensure_user_owns_review
+  # end
 
   # Other scopes may use custom stacks.
   # scope "/api", TestApp do
