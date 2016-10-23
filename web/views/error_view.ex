2a4d6c7df59a9d7c0079b3918e2ce5575676dcf6
Marcle Rodrigues <maarclee@gmail.com>
2016-10-23 16:07:10 -0300

    Add custom error pages

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,7 +2,7 @@ defmodule TestApp.ErrorView do
   use TestApp.Web, :view
 
   def render("404.html", _assigns) do
-    "Page not found"
+    render("not_found.html", %{})
   end
 
   def render("500.html", _assigns) do
